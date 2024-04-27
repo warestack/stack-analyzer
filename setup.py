e0ab8e3102ea946e38124890f7fdfe97715ccb9f
Dimitris Kiriakakis <dkiriakakis@gmail.com>
2024-04-27 04:31:23 +0000

    Increase version number.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="stack_analyzer",
-    version="0.1.0",
+    version="0.1.1",
     author="dimeloper",
     author_email="dkiriakakis@gmail.com",
     description="A library to analyze GitHub repositories and determine their tech stack.",
